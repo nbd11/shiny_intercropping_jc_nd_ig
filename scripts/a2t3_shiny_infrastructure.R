615323bd8e363837a046335545a5c0e91568bac7
jacksoncoldiron <jcoldiron@ucsb.edu>
2025-02-12 20:33:43 -0800

    loading data and script for infrastructure

---
+++ scripts/a2t3_shiny_infrastructure.R
@@ -0,0 +1,11 @@
+# Intercropping Shiny App
+# Assignment 2 Task 3: Shiny Infrastructure
+# Jackson Coldiron, Nicolas DeStephano, Isa Elias
+
+###########################################################
+
+# Load libraries
+
+library(shiny)
+library(tidyverse)
+
